2ba8abdec7305e7723a7315a006cf416a6449417
Edgar Zatarain <edzatarain1123@gmail.com>
2022-04-01 11:19:41 -0500

    added a read me and different classes

--- budget.py
+++ budget.py
@@ -1,30 +1,26 @@
-import os
 import csv
-import pandas as pd 
+import pandas as pd
+from addingStuff import AddingStuff 
 
-class Budget:
+class Budget(AddingStuff):
     def __init__(self, income):
+        super().__init__()
         self.income = income
-        self.categories = self.load_csv()
-    
-    def sub_(self):
-        num = 0
-        for category in self.categories:
-            num += int(category['amount'])
-        return num
+
 
     def add_category(self, dict_):
         self.categories.append(dict_)
+        print(self.categories)
         self.write_csv()
 
     def move_money_category(self, first_category, amount, second_category):
         first_loop = False
         second_loop = False
-        list_categories = []
-        for i in self.categories:
-            list_categories.append(i['category'])
+        # list_categories = []
+        # for i in self.categories:
+        #     list_categories.append(i['category'])
 
-        if second_category in list_categories:
+        if second_category in self.load_csv():
             for first_dict in self.categories:
                 if first_category == first_dict['category']:
                     less_amount = int(first_dict['amount'])-int(amount)
@@ -32,7 +28,7 @@ class Budget:
                     first_loop = True
 
 
-        if first_category in list_categories:
+        if first_category in self.categories:
             for second_dict in self.categories:
                 if second_category == second_dict['category']:
                     new_amount = int(second_dict['amount']) + int(amount)
@@ -63,82 +59,20 @@ class Budget:
         except:
             print('invalid entry')
 
-    def show_categories(self):
+    def show_budget(self):
         print('———— Here is your Budget ————')
         print(f'Your total monthly income is: {self.income} Dollars')
-        for i in self.categories:
+        for i in self.load_csv():
             category = i['category']
             amount = i['amount']
-            print(f'for {category} you can spend {amount} Dollars')
+            print(f' {category} || {amount} Dollars')
         print(f'and you have {self.income - self.sub_()} Dollars left')
         print('——— END ———')
     
     def show_money_left(self):
         print(self.income - self.sub_())
+
+
+        
     
-    def load_csv(self):
-        arr = []
-        path = os.path.abspath(os.path.dirname(__file__))
-        file_path = os.path.join(path, 'data.csv')
-        
-        with open(file_path) as csvfile:
-            reader = csv.DictReader(csvfile)
-            for dict_ in reader:
-                arr.append(dict_)
-        
-        return arr
-
-    def write_csv(self):
-        df = pd.DataFrame.from_dict(self.categories)
-        df.to_csv(r'data.csv', index = False, header = True)
-
-    def menu(self):
-        print('——— Welcome to Your Budget ———')
-
-        text = f'What would you like to do?\n1. Add a New Category\n2. Move Money into Category from Income\n3. Move Money from Category into Another Category\n4. View Budget\n5. Exit\n'
-
-        user_answer = input(text)
-
-        while True:
-            #add a category
-            if user_answer == '1':
-                user_dict_ = {}
-                user_dict_['category'] = input('what is the category\n')
-                user_dict_['amount'] = 0
-                self.add_category(user_dict_)                
-                user_answer = input(text)
-            #move money with income
-            elif user_answer == '2':
-                for i in self.categories:
-                    category = i['category']
-                    print(f'{category} is a category')
-                user_category = input('choose a category from above\n')
-                print(f'{self.income-self.sub_()} here is how much money you have')
-                user_amount = input('choose a whole number amount less than the money you have free\n')
-
-                self.moving_money_income(user_category,user_amount)
-                user_answer = input(text)
-            #move category moneys
-            elif user_answer == '3':
-                for i in self.categories:
-                    category = i['category']
-                    money = i['amount']
-                    print(f'{category} is a category with {money} Dollars')
-                user_category = input('choose category to take from\n')
-                user_amount = input('choose a whole number amount\n')
-                user_new_category = input('choose category to add to\n')
-                self.move_money_category(user_category,user_amount,user_new_category)
-                user_answer = input(text)
-            #show categories/ turns into show budget eventually
-            elif user_answer == '4':
-                self.show_categories()
-                user_answer = input(text)
-  
-            elif user_answer == '5':
-                print('thank you come again')
-                break
-            else:
-                print('that is not an option')
-                user_answer = input(text)
-
-
+    
--- runner.py
+++
@@ -1,6 +0,0 @@
-from budget import Budget
-my_income = 1850*2
-
-my_budget = Budget(my_income)
-
-my_budget.menu()
